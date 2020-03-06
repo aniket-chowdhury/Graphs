ac0d7c5f4d232f758a65de9dddbd0aabed14bf21
Aniket Biprojit Chowdhury <aniket_chowdhury@hotmail.com>
2020-03-06 13:01:05 +0530

    intial graph

---
+++ include/graph.hpp
@@ -0,0 +1,50 @@
+#include "nodes.hpp"
+#include <utility>
+#include <vector>
+#include <initializer_list>
+#include <iostream>
+
+template <typename T>
+std::ostream &operator<<(std::ostream &out, const std::initializer_list<T> l)
+{
+    for (auto it = l.begin(); it < l.end(); it++)
+    {
+        out << *it;
+    }
+    out << "\n";
+    return out;
+}
+
+namespace lib
+{
+template <typename T>
+class graph
+{
+public:
+    std::vector<std::pair<T, GraphNode<T>>> v;
+    graph(std::initializer_list<std::initializer_list<T>> l)
+    {
+        for (auto inner_list = l.begin(); inner_list < l.end(); inner_list++)
+        {
+            T first = *(inner_list->begin());
+            std::vector<T> second(inner_list->begin() + 1, inner_list->end());
+            v.push_back(std::make_pair(first, GraphNode<T>(second)));
+        }
+    };
+
+    friend std::ostream &operator<<(std::ostream &out, const graph g)
+    {
+        for (auto const &p : g.v)
+        {
+            out << p.first;
+            for (auto const i : p.second.list)
+            {
+                out << "->" << i;
+            }
+            out << "\n";
+        }
+        return out;
+    }
+};
+
+} // namespace lib
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include "graph.hpp"
+
+int main(int argc, char *argv[])
+{
+	lib::graph g({{1, 2, 3, 5},
+				  {2, 1, 3, 4},
+				  {3, 1, 2, 4, 5},
+				  {4, 2, 3},
+				  {5, 1, 3}});
+	std::cout << g;
+}
